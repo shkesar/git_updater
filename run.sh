a4245804efa88be41957b0317744c817c0fb01e2
Shubham Keserwani <shubhamkesar@live.com>
2015-02-12 01:02:55 +0530

    Optimised run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+prgname=gitud
+testrepospath=test/repos
+
 cc -c -std=c89 -o build/obj/main.o src/main.c
-cc -all_load -std=c89 -lgit2 -o build/prog build/obj/main.o
-./build/prog test/repos/
+cc -all_load -std=c89 -lgit2 -o build/$prgname build/obj/main.o
+./build/$prgname $testrepospath
